68f4841980295a7d64b3bd0141bce53732accaa2
jplusequalt <jplusequalt@gmail.com>
2022-09-20 18:35:07 -0500

    refactored Drawer styles into Drawer.styled

---
+++ src/styles/SideMenu.styled.tsx
@@ -0,0 +1,14 @@
+import { Drawer, styled } from "@mui/material";
+import { theme } from "../Theme";
+
+export const BoardMenuList = styled(Drawer, { shouldForwardProp: (prop) => prop !== 'drawerWidth' })<{
+  drawerWidth: number
+}>(({ drawerWidth, theme }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  ['& .MuiDrawer-paper']: {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+    backgroundColor: theme.palette.background.paper
+  }
+}));
